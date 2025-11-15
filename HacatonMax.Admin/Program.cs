e1d2df954aefde7678ef1efce975b95bc420f062
Anton <74132592+AntonRls@users.noreply.github.com>
2025-11-15 05:21:35 +0300

    Добавлена конвертация enumov в строку

--- HacatonMax.Admin/Program.cs
+++ HacatonMax.Admin/Program.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using HacatonMax.Admin.Infrastructure;
 using HacatonMax.Admin.Middleware;
 using Microsoft.OpenApi.Models;
@@ -18,6 +19,7 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
     options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.SnakeCaseLower;
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
